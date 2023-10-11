1811b72c734b6780330e74aab3fbf7e10ace2db0
Akash <ash.das38555@gmail.com>
2023-10-11 20:20:14 +0530

    Facebook Clone Project commited

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      'width':{
+       '98' : '980px',
+       '32'   : '320px',
+        '396' :  '396px',
+        '336' :  '336px',
+      }
+    },
+  },
+  plugins: [],
+}
+
